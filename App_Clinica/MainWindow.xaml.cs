b68a0313d2c75f4e4bf7bd04fa420a1ae24a3f59
Alef1412 <102564597+Alef1412@users.noreply.github.com>
2023-04-26 22:38:24 -0300

    create to classes vielModels

--- App_Clinica/MainWindow.xaml.cs
+++ App_Clinica/MainWindow.xaml.cs
@@ -24,25 +24,6 @@ namespace App_Clinica
         {
             InitializeComponent();
         }
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-                DragMove();
-        }
-
-        private void btnMinimize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void btnClose_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show($"Usuário: {txtUser.Text} Senha: {txtPassword.Password}");
-        }
+        
     }
 }
