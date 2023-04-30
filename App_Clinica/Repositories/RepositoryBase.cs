0fa465d60ce0b45063f8cffef132a355da9a5943
Alef1412 <102564597+Alef1412@users.noreply.github.com>
2023-04-29 22:38:16 -0300

    qualquer

---
+++ App_Clinica/Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Clinica.Repositories
+{
+    public class RepositoryBase
+    {
+        private readonly string _connectionString;
+
+        public RepositoryBase()
+        {
+            _connectionString = "Server = DESKTOP-32GK00E\\SQLEXPRESS; Database = Clinica; Integrated Security = true";
+        }
+
+        protected SqlConnection GetConnection() { 
+            return new SqlConnection(this._connectionString);
+        }
+    }
+}
---
+++ App_Clinica/Repositories/UserRepository.cs
@@ -0,0 +1,71 @@
+﻿using App_Clinica.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App_Clinica.Repositories
+{
+    public class UserRepository : RepositoryBase, IUserRepository
+    {
+        public void Add(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+                        
+            try
+            {
+                var connection = GetConnection();
+                using (var command = new SqlCommand()) {
+                command.Connection = connection;
+                    connection.Open();
+                    command.CommandText = "select * from Usuario where Username = '@username' and [Password]= '@password'";
+                    command.Parameters.Add("@username", SqlDbType.NVarChar).Value = credential.UserName;
+                    command.Parameters.Add("@password", SqlDbType.NVarChar).Value = credential.Password;
+
+                    return command.ExecuteScalar() == null ? false : true;
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Ocorreu um erro {ex}");
+                return false;
+            }
+        }
+
+        public void Edit(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
